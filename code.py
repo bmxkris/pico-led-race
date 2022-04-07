8c0410c1efd8142cf0f94a04c41b0a0d2a0530da
bmxkris <kgflint@gmail.com>
2022-04-07 20:45:44 +0100

    Add LCD library and game timer

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
-# Write your code here :
-
 import board
 import neopixel
 import digitalio
+import time
 from adafruit_led_animation.animation.blink import Blink
 from adafruit_led_animation.animation.pulse import Pulse
 from adafruit_led_animation.animation.comet import Comet
@@ -12,30 +11,31 @@ from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.group import AnimationGroup
 from adafruit_led_animation.color import RED, GREEN, BLUE, WHITE, YELLOW, PURPLE, AMBER
 from adafruit_debouncer import Debouncer
-
+import fourdigitsevensegmentLEDdisplay as led_display
 
 num_pixels = 90
-play_to = 90  # or num_pixels
-brightness = 1
+play_to = 3  # or num_pixels
+brightness = 0.1
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 
-def winner_animation(pixels, pixels2, button1, button2, reset_button):
+def winner_animation(pixels, pixels2, button1, button2, reset_button, time_played):
     animations = AnimationSequence(
-        Blink(pixels, speed=0.07, color=WHITE), 
-        Comet(pixels, speed=0.0082, color=PURPLE, tail_length=10, bounce=True), 
+        Blink(pixels, speed=0.07, color=WHITE),
+        Comet(pixels, speed=0.0082, color=PURPLE, tail_length=10, bounce=True),
         Rainbow(pixels, speed=0.01, period=1.5),
         Rainbow(pixels, speed=0.01, period=1.5),
         Rainbow(pixels, speed=0.01, period=1.5),
         advance_interval=1.5,
         auto_clear=True,
     )
-    
+
     while animations.cycle_count < 1 :
+        led_display.display_number(time_played)
         animations.animate()
-        
+
     return True
 
 
@@ -71,20 +71,23 @@ def init_game(pixels, pixels2, button1, button2, reset_button):
         advance_interval=1.5,
         auto_clear=True,
     )
-    
+
     while animations.cycle_count < 1:
         animations.animate()
-    
+
     pixels.fill(RED)
     pixels2.fill(BLUE)
     pixels.show()
     pixels2.show()
-    
+
     global play_to
     player1 = 0
     player2 = 0
     game_over = False
-    
+
+    #start time
+    start_time = time.monotonic()
+
     while not game_over:
         led.value = True
         button1.update()
@@ -96,34 +99,41 @@ def init_game(pixels, pixels2, button1, button2, reset_button):
         if button2.fell:
             player2 = increment_pixels(pixels2, player2)
 
+        time_played = time.monotonic() - start_time
         if player1 == play_to:
-            game_over = winner_animation(pixels, pixels2, button1, button2, reset_button)
+            game_over = winner_animation(pixels, pixels2, button1, button2, reset_button, time_played)
 
         elif player2 == play_to:
-            game_over = winner_animation(pixels2, pixels, button1, button2, reset_button)
-    
+            game_over = winner_animation(pixels2, pixels, button1, button2, reset_button, time_played)
+
     standby_animation(pixels, pixels2, button1, button2, reset_button)
 
-def standby_animation(pixels, pixels2, button1, button2, reset_button): 
+def standby_animation(pixels, pixels2, button1, button2, reset_button):
     sparkle = AnimationGroup(
         Sparkle(pixels, speed=0.1, color=AMBER, num_sparkles=10),
         Sparkle(pixels2, speed=0.1, color=AMBER, num_sparkles=10),
     )
-    
+
     while True:
         reset_button.update()
         # standby animation
         sparkle.animate()
-        
+
         if reset_button.fell:
             init_game(pixels, pixels2, button1, button2, reset_button)
 
+def setup_and_standby():
+    pixels = neopixel.NeoPixel(board.GP2, num_pixels, brightness=brightness, auto_write=False)
+    pixels2 = neopixel.NeoPixel(board.GP3, num_pixels, brightness=brightness, auto_write=False)
 
-pixels = neopixel.NeoPixel(board.GP2, num_pixels, brightness=brightness, auto_write=False)
-pixels2 = neopixel.NeoPixel(board.GP3, num_pixels, brightness=brightness, auto_write=False)
+    reset_button = create_button(board.GP28)
+    button1      = create_button(board.GP26)
+    button2      = create_button(board.GP27)
 
-reset_button = create_button(board.GP14)
-button1      = create_button(board.GP16)
-button2      = create_button(board.GP17)
+    standby_animation(pixels, pixels2, button1, button2, reset_button)
 
-standby_animation(pixels, pixels2, button1, button2, reset_button)
+if __name__ == '__main__': #Program starting from here
+
+    setup_and_standby()
+
+
